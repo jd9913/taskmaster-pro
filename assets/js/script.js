2b21a9ca929097cdd32bc8a4123eea805f52e2e6
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-26 13:33:17 -0700

    adding drag drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,12 +43,81 @@ var loadTasks = function () {
 
 var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    $(".list-group").on("click", "p", function () {
-        console.log(this);
-    });
 };
 
+//enable draggable/sortable feature on list-group elements
 
+$(".card .list-group").sortable({
+    //enable dragging across lists
+    connectWith: $(".car .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function (event, ui) {
+        console.log(ui);
+    },
+    deactivate: function (event, ui) {
+        console.log(ui);
+    },
+    over: function (event) {
+        console.log(event);
+    },
+    out: function (event) {
+        console.log(event);
+    },
+    update: function (event) {
+        console.log($(this).children());
+
+        var tempArr = [];
+        //loop over current set of childern in sortable list
+
+        $(this)
+            .children()
+            .each(function () {
+                console.log($(this));
+                //save values in temp array
+                tempArr.push({
+                    text: $(this)
+                        .find("p")
+                        .text()
+                        .trim(),
+                    date: $(this)
+                        .find("span")
+                        .text()
+                        .trim()
+                });
+            });
+        //trim down list's ID to match object property
+        var arrName = $(this)
+            .attr("id")
+            .replace("list-", "");
+
+        //update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+    },
+    stop: function (event) {
+        $(this).removeClass("dropover");
+    }
+});
+
+//trash icon can be dropped onto
+
+$("#trash").droppable( {
+    accept: ".card .list-group-item",
+        tolerance: "touch",
+    drop: function (event, ui) {
+        console.log("drop");
+            //remove dragged element from the dom
+            ui.draggable.remove()
+    },
+    over: function(event, ui) {
+        console.log(ui);
+    },
+    out: function(event, ui) {
+        console.log(event);
+    }
+});
 
 
 
